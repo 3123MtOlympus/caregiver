8a39c38f2c02a93870bc28fef93e14a7e990b23e
3123MtOlympus <realchrislondon@gmail.com>
2024-07-18 13:55:10 -0700

    added function to book consultation btn

--- asset/script.js
+++ asset/script.js
@@ -76,6 +76,33 @@ document.getElementById('submit-review').addEventListener('click', function() {
     }
 });
 
+// Get the modal
+var modal = document.getElementById("booking-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("book-consultation-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 function bookMonthlyPackage() {
     alert("You have selected the Monthly Package. Proceed to payment.");
     // Implement the booking logic here, e.g., redirect to payment page
